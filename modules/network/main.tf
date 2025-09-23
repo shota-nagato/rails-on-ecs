418fe59ac4de517310a83d148f51d659af44e94b
shota-nagato <shota.nagato@gmail.com>
2025-09-23 10:25:58 +0900

    vpcにタグを追加

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -2,4 +2,8 @@ resource "aws_vpc" "main" {
   cidr_block           = var.network.vpc_cidr
   enable_dns_hostnames = true
   enable_dns_support   = true
+
+  tags = {
+    Name = "${var.common.prefix}-${var.common.environment}-vpc"
+  }
 }
